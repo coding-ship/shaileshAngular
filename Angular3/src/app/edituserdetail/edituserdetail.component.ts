eb92f597b5954dfe7a76991cf357258627d76c45
Shubham <311297sg@gmail.com>
2021-01-09 21:58:30 -0800

    first

---
+++ Angular3/src/app/edituserdetail/edituserdetail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customer';
+
+@Component({
+  selector: 'app-edituserdetail',
+  templateUrl: './edituserdetail.component.html',
+  styleUrls: ['./edituserdetail.component.css']
+})
+export class EdituserdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onsubmit(customer:Customer){
+    window.alert("Account Successfully Created Now You Can Login!");
+    console.log(customer);
+    console.log(customer.fname);
+    console.log(customer.lname);
+    console.log(customer.phone);
+    console.log(customer.role);
+    
+
+  }
+
+
+}
